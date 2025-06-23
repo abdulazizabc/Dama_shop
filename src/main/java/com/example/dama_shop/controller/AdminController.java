996dd481b4d842275fa472f43ef34c75ea538780
abdulaziz <dedmoroz.deda1@gmail.com>
2025-06-23 21:13:29 +0300

    small upgrade

--- src/main/java/com/example/dama_shop/controller/AdminController.java
+++ src/main/java/com/example/dama_shop/controller/AdminController.java
@@ -8,6 +8,7 @@ import com.example.dama_shop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +17,7 @@ import java.util.List;
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 @Slf4j
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final UserService userService;
