9b6210632aad36d77e5fe4c4499dab1bdd71838b
abdulaziz <dedmoroz.deda1@gmail.com>
2025-06-23 21:12:40 +0300

    small upgrade

--- src/main/java/com/example/dama_shop/dto/OrderDTO.java
+++ src/main/java/com/example/dama_shop/dto/OrderDTO.java
@@ -2,12 +2,21 @@ package com.example.dama_shop.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class OrderDTO {
-
+    @NotNull
     private Long user_id;
+    @NotBlank
     private String orderName;
-    private int price;
+    @Min(0)
+    @Max(1_000_000_000)
+    private double price;
+    @NotBlank
     private String status;
 
 }
